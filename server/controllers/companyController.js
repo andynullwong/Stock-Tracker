5a846bbc1c52e8d4c71ce286c8ef70975cc7fab1
Andy Wong <andywong.ny@gmail.com>
2019-08-21 19:49:14 -0400

    completed MongoDB entry

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -3,12 +3,15 @@ const keys = require("../../keys.js");
 const Company = require("../models/company.js");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/", (err, db) => {
-  console.log("MongoDB Connection opened on Port 27017");
-});
+mongoose.connect(
+  "mongodb://localhost:27017/stockdb",
+  { useNewUrlParser: true },
+  (err, db) => {
+    console.log("MongoDB Connection opened on port 27017");
+  }
+);
 
 const companyController = {};
-
 companyController.addCompany = (req, res, next) => {
   fetch(
     `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${
@@ -21,12 +24,20 @@ companyController.addCompany = (req, res, next) => {
       inputArray.forEach(day => priceArray.push(day["1. open"]));
 
       const company = new Company({
-        _id: new mongoose.Types.ObjectId(),
+        // _id: new mongoose.Types.ObjectId(),
         ticker: data["Meta Data"]["2. Symbol"],
         prices: priceArray
       });
-      //   console.log(company);
-      db("stockmarket")
+
+      Company.findOneAndUpdate(
+        { ticker: company.ticker },
+        company,
+        { upsert: true, useFindAndModify: true },
+        (err, res) => {
+          if (err) throw err;
+          console.log("Record Updated:", company.ticker);
+        }
+      );
     })
   );
   next();
--- server/models/company.js
+++ server/models/company.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const companySchema = new Schema({
   _id: mongoose.Schema.Types.ObjectId,
   ticker: String,
